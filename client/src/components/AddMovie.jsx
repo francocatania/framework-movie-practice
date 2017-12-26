542ba4a81a342f0d4ab02b149e1929f8e4e4aede
Franco <frc.catani@gmail.com>
2017-12-25 16:04:38 -0800

    placeholders corrected

--- client/src/components/AddMovie.jsx
+++ client/src/components/AddMovie.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function AddMovie(props) {
   return (
     <div className='AddMovie'>
-      <input className="addMovieBar" type="text" placeholder="Type a Title and press Add" onChange={props.handleChange} value={props.inputValue}/>
+      <input className="addMovieBar" type="text" placeholder="Add a movie title" onChange={props.handleChange} value={props.inputValue}/>
       <button className="addMovieButton" type="button" onClick={props.clickHandler}>Add</button>
     </div>
   );
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Search(props) {
   return (
     <div className='Search'>
-      <input className='searchBar' type='text' placeholder='Search for a movie' onChange={props.handleChange}/>
+      <input className='searchBar' type='text' placeholder='Search...' onChange={props.handleChange}/>
     </div>
   );
 }
